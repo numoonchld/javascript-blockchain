ce3599bfc5c3df79bd8643c71afbefbc13710ebf
Raghavendra Saralaya <17598505+numoonchld@users.noreply.github.com>
2023-02-14 23:44:24 +0530

    transactions collecting into blocks

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,6 +1,6 @@
 function Blockchain() {
     this.chain = []
-    this.newTransactions = []
+    this.pendingTransactions = []
 }
 
 /*
@@ -12,24 +12,38 @@ class Blockchain {
 }
 */
 
+// collects all validated transactions and is set in stone into the blockchain
 Blockchain.prototype.createNewBlock = function (nonce, previousBlockHash, hash) {
     const newBlock = {
         index: this.chain.length + 1,
         timestamp: Date.now(),
-        transactions: this.newTransactions,
+        transactions: this.pendingTransactions,
         nonce: nonce, // comes from proof-of-work, can be any number
         hash: hash,
         previousBlockHash: previousBlockHash,
     }
 
-    this.newTransactions = []
+    this.pendingTransactions = []
     this.chain.push(newBlock)
 
     return newBlock
 }
 
 Blockchain.prototype.getLastBlock = function() {
-    return this.chain[this.chain.length - 1 ]
+    return this.chain[this.chain.length - 1]
+}
+
+// these transactions are not set in stone until a new block is mined
+Blockchain.prototype.createNewTransaction = function(amount, sender, recipient) {
+    const newTransaction = {
+        amount,
+        sender,
+        recipient
+    }
+
+    this.pendingTransactions.push(newTransaction)
+
+    return this.getLastBlock()['index'] + 1
 }
 
 module.exports = Blockchain
--- dev/test.js
+++ dev/test.js
@@ -1,9 +1,21 @@
 const Blockchain = require('./blockchain')
 
 const luzcoin = new Blockchain()
-luzcoin.createNewBlock(0,'akjdf6astfhs8f6gw4','u4378rch234cr4')
-luzcoin.createNewBlock(34,'akjdf6fashs8f6gw4','llms744ddasdf234cr4')
-luzcoin.createNewBlock(54,'akjdf6a323hs8f6gw4','akjjlsjdcbjh434')
 
+// luzcoin.createNewBlock(0,'akjdf6astfhs8f6gw4','u4378rch234cr4')
+// luzcoin.createNewBlock(34,'akjdf6fashs8f6gw4','llms744ddasdf234cr4')
+// luzcoin.createNewBlock(54,'akjdf6a323hs8f6gw4','akjjlsjdcbjh434')
 
-console.log(luzcoin)
+luzcoin.createNewBlock(578423, 'ASK253FWFSDA34', 'F34FF34FWERGE')
+
+luzcoin.createNewTransaction(100, 'xA23LAF4457FD', 'xFSDS78FDS69')
+
+luzcoin.createNewBlock(345324, 'AGFS253SDFGSDA34', 'SD33FFF34FWEE')
+
+luzcoin.createNewTransaction(10, 'xA23LAF4457FD', 'xFSDS78FDS69')
+luzcoin.createNewTransaction(100, 'xA23LAF4457FD', 'xFSDS78FDS69')
+luzcoin.createNewTransaction(5000, 'xA23LAF4457FD', 'xFSDS78FDS69')
+
+luzcoin.createNewBlock(41234, 'SERF253SDFGSDA34', 'HFSD3FFASD4FWEE')
+
+console.log(luzcoin, '\n\n\n', luzcoin.chain[1], '\n\n\n', luzcoin.chain[2], )
