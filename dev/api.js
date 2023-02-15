bdf110f618549edaa7c525df1b2b764eec64d2d5
Raghavendra Saralaya <17598505+numoonchld@users.noreply.github.com>
2023-02-15 22:35:01 +0530

    setup nodemon and parsing json with express

--- dev/api.js
+++ dev/api.js
@@ -1,20 +1,30 @@
 const express = require('express')
 const app = express()
 
+// https://stackoverflow.com/a/49943829
+app.use(express.json())
+// https://www.geeksforgeeks.org/express-js-express-urlencoded-function/
+app.use(express.urlencoded({ extended: false }));
+
 // landing page api
-app.get('/', function(req,res) {
+app.get('/', function (req, res) {
     res.send('Luz Coin')
 })
 
 // endpoint to get entire blockchain
-app.get('/blockchain', function(req,res0) {})
+app.get('/blockchain', function (req, res) { })
 
 // endpoint to post a new transaction
-app.post('/transaction', function(req,res0) {})
+app.post('/transaction', function (req, res) {
+    console.log(req.body)
+    res.send(`Transaction amount: ${req.body.amount}`)
+})
 
 // endpoint to trigger mining of a new block
-app.get('/mine', function(req,res0) {})
+app.get('/mine', function (req, res) { })
 
-app.listen(3000, function() {
+
+// setup the listener @ port 3000
+app.listen(3000, function () {
     console.log('listening on port 3000...')
 })
