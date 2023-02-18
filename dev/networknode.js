dd89ba80d9b7dbd2c58880b2b1404b2d31af096a
Raghavendra Saralaya <17598505+numoonchld@users.noreply.github.com>
2023-02-18 21:59:40 +0530

    making nodes attach to network

--- dev/networknode.js
+++ dev/networknode.js
@@ -7,6 +7,7 @@ const { v4: uuidv4 } = require('uuid');
 const nodeAddress = uuidv4().split('-').join('')
 console.log(nodeAddress)
 const port = process.argv[2]
+const rp = require('request-promise')
 
 const express = require('express')
 const app = express()
@@ -65,15 +66,69 @@ app.get('/mine', function (req, res) {
 
 
 // register a node and broadcast network node to the rest of the network
-app.post('/register-and-broadcast-node', function (req, res) {
+app.post('/register-and-broadcast-node', async function (req, res) {
     const newNodeURL = req.body.newNodeURL
+
+    // add network node http address into luzcoin data structure
+    if (luzcoin.networkNodes.indexOf(newNodeURL) === -1) luzcoin.networkNodes.push(newNodeURL)
+
+    // then broadcast into other nodes
+    const registerNodesPromises = []
+    luzcoin.networkNodes.forEach(networkNodeURL => {
+        const requestOptions = {
+            uri: `${networkNodeURL}/accept-incoming-node`,
+            method: 'POST',
+            body: { newNodeURL },
+            json: true
+
+        }
+
+        registerNodesPromises.push(rp(requestOptions))
+    })
+
+    // send all existing nodes in bulk to newly added node
+    await Promise.all(registerNodesPromises).then(data => {
+
+        const bulkRequestOptions = {
+            uri: `${newNodeURL}/accept-existing-nodes-bulk`,
+            method: 'POST',
+            body: { networkNodes: [...luzcoin.networkNodes, luzcoin.currentNodeURL] },
+            json: true
+
+        }
+        return rp(bulkRequestOptions)
+    }).then(data => {
+        res.json({
+            message: "New node registered with network successfully!"
+        })
+    })
 })
 
 // accept a new node broadcast to the network
-app.post('/accept-incoming-node', function (req, res) { })
+app.post('/accept-incoming-node', function (req, res) {
+    const newNodeURL = req.body.newNodeURL
+
+    const nodeNotAlreadyPresent = luzcoin.networkNodes.indexOf(newNodeURL) === -1
+    const nodeNotCurrentOne = luzcoin.currentNodeURL !== newNodeURL
+
+    if (nodeNotAlreadyPresent && nodeNotCurrentOne) luzcoin.networkNodes.push(newNodeURL)
+    res.json({
+        message: 'new node registered successfully with this node!'
+    })
+})
 
 // accept a bulk of existing nodes from the network to the newly attached node
-app.post('/accept-existing-nodes-bulk', function (req, res) { })
+app.post('/accept-existing-nodes-bulk', function (req, res) {
+    const networkNodes = req.body.networkNodes
+    networkNodes.forEach(networkNodeURL => {
+        const nodeNotAlreadyPresent = luzcoin.networkNodes.indexOf(networkNodeURL) === -1
+        const nodeNotCurrentOne = luzcoin.currentNodeURL !== networkNodeURL
+        if (nodeNotAlreadyPresent && nodeNotCurrentOne) luzcoin.networkNodes.push(networkNodeURL)
+    })
+    res.json({
+        message: 'bulk registration successful!'
+    })
+})
 
 // setup the listener @ port 3000
 app.listen(port, function () {
